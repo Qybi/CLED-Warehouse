7453c86feafbadc8114e533a00ec1b46ab8854b8
ilMett <152792531+ilMett@users.noreply.github.com>
2024-04-09 23:29:22 +0200

    Connection Prova

--- CLED.Warehouse.Web/CLED.WareHouse.Services/DBServices/TicketService.cs
+++ CLED.Warehouse.Web/CLED.WareHouse.Services/DBServices/TicketService.cs
@@ -1,32 +1,41 @@
-﻿using CLED.WareHouse.Models.Database;
+﻿using System.Data;
+using CLED.WareHouse.Models.Database;
 using CLED.WareHouse.Services.DBServices.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace CLED.WareHouse.Services.DBServices;
 
 public class TicketService : IService<Ticket>
 {
+    private readonly IDbConnection _connection;
 
-    public async Task<Ticket> GetById(int id)
+    public TicketService(IConfiguration configuration)
+    {
+        _connection = new NpgsqlConnection(configuration.GetConnectionString("db"));
+    }
+
+    public Task<Ticket> GetById(int id)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<IEnumerable<Ticket>> GetAll()
+    public IEnumerable<Task<Ticket>> GetAll()
     {
         throw new NotImplementedException();
     }
 
-    public async Task Insert(Ticket ticket)
+    public Task Insert(Ticket ticket)
     {
         throw new NotImplementedException();
     }
 
-    public async Task Update(Ticket ticket)
+    public Task Update(Ticket ticket)
     {
         throw new NotImplementedException();
     }
 
-    public async Task Delete(int id)
+    public Task Delete(int id)
     {
         throw new NotImplementedException();
     }
