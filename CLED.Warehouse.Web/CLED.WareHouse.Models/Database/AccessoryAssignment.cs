ee94d5c267fc1a8b299fef5e0360a3492549c6e4
ilMett <152792531+ilMett@users.noreply.github.com>
2024-04-09 16:25:29 +0200

    Creation Some Models

---
+++ CLED.Warehouse.Web/CLED.WareHouse.Models/Database/AccessoryAssignment.cs
@@ -0,0 +1,18 @@
+﻿namespace CLED.WareHouse.Models.Database;
+
+public class AccessoryAssignment
+{
+    public int Id { get; set; }
+    public int AccessoryId { get; set; }
+    public int StudentId { get; set; }
+    public DateTime AssignmentDate { get; set; } = DateTime.Now;
+    public int AssignmentReasonId { get; set; }
+    public bool IsReturned { get; set; } = false;
+    public DateTime ForecastReturnDate { get; set; } = default!;
+    public DateTime? ActualReturnDate { get; set; }
+    public int? ReturnReasonId { get; set; }
+    public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    public string RegistrationUser { get; set; } = default!;
+    public DateTime? DeletedDate { get; set; }
+    public string? DeletedUser { get; set; }
+}
---
+++ CLED.Warehouse.Web/CLED.WareHouse.Models/Database/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace CLED.WareHouse.Models.Database;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = default!; // lunghezza di 30 char {es CLED, SOQU}
+    public string FullName { get; set; } = default!;
+    public DateTime DateStart { get; set; } = default!;
+    public DateTime DateEnd { get; set; } = default!; //{massimo due anni di durata del corso}
+    public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    public DateTime RegistrationUser { get; set; } = default!;
+    public DateTime? DeletedDate { get; set; }
+    public string? DeletedUser { get; set; }        
+}
