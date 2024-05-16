49aa8e5d28ff29b07dda708163ca169033529393
Luca Bernardis <lucabusinesswork1@gmail.com>
2024-05-16 10:20:12 +0200

    Adjust bls i made

--- CLED.Warehouse.Web/CLED.WareHouse.Models/FileUpload/UploadedStudentsJson.cs
+++ CLED.Warehouse.Web/CLED.WareHouse.Models/FileUpload/UploadedStudentsJson.cs
@@ -2,5 +2,29 @@ namespace CLED.WareHouse.Models.FileUpload;
 
 public class UploadedStudentsJson
 {
-    
+    public JsonStudentModel[] Rows { get; set; }
+}
+
+public class JsonStudentModel
+{
+    public string IdAllievoCorso { get; set; } = default!;
+    public string CodiceFiscale { get; set; } = default!;
+    public string SiglaCorso { get; set; } = default!;
+    public string CodiceCorso { get; set; } = default!;
+    public string NomeCorso { get; set; } = default!;
+    public string Cognome { get; set; } = default!;
+    public string Nome { get; set; } = default!;
+    public string EmailUser { get; set; } = default!;
+    public string Tel { get; set; } = default!;
+    public string ComuneResidenza { get; set; } = default!;
+    public string ProvinciaResidenza { get; set; } = default!;
+    public string StatoAllievo { get; set; } = default!; 
+    public string StatoCorso { get; set; } = default!;
+    public string AnnoCorso { get; set; } = default!;
+    public string DataNascita { get; set; } = default!;
+    public string ComuneNascita { get; set; } = default!;
+    public string ProvinciaNascita { get; set; } = default!;
+    public string? DataDimissioni { get; set; }
+    public string Genere { get; set; } = default!;
+    public string NazioneNascita { get; set; } = default!;
 }
